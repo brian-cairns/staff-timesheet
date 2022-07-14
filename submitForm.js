fafdafdc8e5a70fafb22d0f21edcf5297b01b81e
brian-cairns <79775364+brian-cairns@users.noreply.github.com>
2022-07-14 11:56:34 -0400

    never use webflow for inputs!!!

--- submitForm.js
+++ submitForm.js
@@ -27,13 +27,13 @@ supervisorName.addEventListener('change', (e) => {
   console.log(newForm.supervisorName);
 })
 
-let date = document.querySelector('input#date')
-date.addEventListener('change', (e) => {
+let todaysDate = document.querySelector('input#todaysDate')
+todaysDate.addEventListener('change', (e) => {
 	console.log('changed')
 	newForm.date = e.target.value;
-  console.log(newForm.date);
+  console.log(newForm.todaysDate);
 })
-  
+ 
 class TimeEntry {
     constructor(date, timeIn, timeOut, ttlHrs, goal) {
         this.date = date;
@@ -44,49 +44,133 @@ class TimeEntry {
     }
 }
 
-const getTime = new Promise((res) => {
-    let time = []
-    for (let i = 1; i < 5; i++) {
-        if (document.getElementById(`date${i}`).value = '') {
-            i = 5
-            return time
-        }
-        let item = new TimeEntry
-        item.date = document.getElementById(`date${i}`).value;
-        let timeIn = document.getElementById(`timeIn${i}`).value;
-        let am = document.getElementById(`am${i}`).value;
-        let timeOut = document.getElementById(`timeOut${i}`).value;
-        let pm = document.getElementById(`pm${i}`).value;
-        item.timeIn = await get24hr(timeIn, am)
-        item.timeOut = await get24hr(timeOut, pm)
-        item.ttlHrs = document.getElementById(`ttlHrs${i}`).value
-        item.goal = document.getElementById(`goal${i}`).value
-        time.push(item)
-    }
-    res(time)
-})
-
-const getTtlHrs = new Promise ((res) => {
-    let ttlHrs = 0
-    for (let i = 1; i < 5; i++) {
-        ttlHrs = ttlHrs + document.getElementById(`ttlHrs${i}`)
-    }
-    res(ttlHrs)
-})
-
-function get24hr(time, part) {
-    let timeArray = time.parse(':')
-    let hour = parseInt(timeArray[0])
-    part == 'PM' ? hour = hour + 12 : hour
-    timeArray[0] = hour.toString
-    let time24 = `${timeArray[0]}:${timeArray[1]}`
-    return time24
+let timeDetails = []
+for (let i = 1; i < 5; i++) {
+  timeDetails[i] = new TimeEntry
 }
 
+let date1 = document.querySelector('input#date1')
+date1.addEventListener('change', (e) => {
+  timeDetails[1].date = e.target.value
+  console.log(e.target.value)
+})
+
+let in1 = document.querySelector('input#in1')
+in1.addEventListener('change', (e) => {
+  timeDetails[1].timeIn = e.target.value
+  console.log(e.target.value)
+})
+
+let out1 = document.querySelector('input#out1')
+out1.addEventListener('change', (e) => {
+  timeDetails[1].timeOut = e.target.value
+  console.log(e.target.value)
+})
+
+let goal1 = document.querySelector('input#goal1')
+goal1.addEventListener('change', (e) => {
+  timeDetails[1].goal = e.target.value
+  console.log(e.target.value)
+})
+
+let ttlHrs1 = document.querySelector('input#ttlHrs1')
+ttlHrs1.addEventListener('change', (e) => {
+  timeDetails[1].ttlHrs = parseFloat(e.target.value)
+  console.log(e.target.value)
+})
+
+let date2 = document.querySelector('input#date2')
+date2.addEventListener('change', (e) => {
+  timeDetails[2].date = e.target.value
+  console.log(e.target.value)
+})
+
+let in2 = document.querySelector('input#in2')
+in2.addEventListener('change', (e) => {
+  timeDetails[2].timeIn = e.target.value
+  console.log(e.target.value)
+})
+
+let out2 = document.querySelector('input#out2')
+out2.addEventListener('change', (e) => {
+  timeDetails[2].timeOut = e.target.value
+  console.log(e.target.value)
+})
+
+let goal2 = document.querySelector('input#goal2')
+goal2.addEventListener('change', (e) => {
+  timeDetails[2].goal = e.target.value
+  console.log(e.target.value)
+})
+
+let ttlHrs2 = document.querySelector('input#ttlHrs2')
+ttlHrs2.addEventListener('change', (e) => {
+  timeDetails[2].ttlHrs = parseFloat(e.target.value)
+  console.log(e.target.value)
+})
+
+let date3 = document.querySelector('input#date3')
+date3.addEventListener('change', (e) => {
+  timeDetails[3].date = e.target.value
+})
+
+let in3 = document.querySelector('input#in3')
+in3.addEventListener('change', (e) => {
+  timeDetails[3].timeIn = e.target.value
+})
+
+let out3 = document.querySelector('input#out3')
+out3.addEventListener('change', (e) => {
+  timeDetails[3].timeOut = e.target.value
+})
+
+let goal3 = document.querySelector('input#goal3')
+goal3.addEventListener('change', (e) => {
+  timeDetails[3].goal = e.target.value
+})
+
+let ttlHrs3 = document.querySelector('input#ttlHrs3')
+ttlHrs3.addEventListener('change', (e) => {
+  timeDetails[3].ttlHrs = parseFloat(e.target.value)
+})
+
+let date4 = document.querySelector('input#date4')
+date4.addEventListener('change', (e) => {
+  timeDetails[4].date = e.target.value
+})
+
+let in4 = document.querySelector('input#in4')
+in4.addEventListener('change', (e) => {
+  timeDetails[4].timeIn = e.target.value
+})
+
+let out4 = document.querySelector('input#out4')
+out4.addEventListener('change', (e) => {
+  timeDetails[4].timeOut = e.target.value
+})
+
+let goal4 = document.querySelector('input#goal4')
+goal4.addEventListener('change', (e) => {
+  timeDetails[4].goal = e.target.value
+})
+
+let ttlHrs4 = document.querySelector('input#ttlHrs4')
+ttlHrs4.addEventListener('change', (e) => {
+  timeDetails[4].ttlHrs = parseFloat(e.target.value)
+})
+
+let totalHrs = document.getElementById('totalHrs')
+totalHrs.addEventListener('click', () => {
+  let sum = 0
+  for (let i = 1; i < 5; i++) {
+    typeof(timeDetails[i].ttlHrs) == "number" ? sum = sum + timeDetails[i].ttlHrs : i = 5
+  }
+  document.getElementById('sumHrs').innerHTML = sum
+})
+
 document.getElementById('submit').addEventListener("click", async (event) => {
-    newForm.timeDetails = await getTime
-    newForm.ttlHrs = await getTtlHrs
-    document.getElementById('sumHrs').innerHTML = getTtlHrs
+    newForm.timeDetails = timeDetails
+    console.log(newForm)
     submitForm(newForm, formName)
 })
 
